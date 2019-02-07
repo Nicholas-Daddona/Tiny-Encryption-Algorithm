fa1ebe740b2762a8632f3ca71db1effab4c8867d
Nick Daddona <ndaddona@my.ccsu.edu>
2019-02-06 21:23:56 -0500

    Initial Commit
    Add temporary files for directory structure and configuration files.

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestMain {
+
+    // TODO: Remove
+    @Test
+    public void testMain() {
+        Assert.assertTrue(true);
+    }
+}
